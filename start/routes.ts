81899e05b36a607b0e754eadbcb95c7166ce2b7e
EnzoWBR <ti02.awf@gmail.com>
2024-10-03 10:49:57 -0300

    routes funcionando

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+import AlunosController from '#controllers/alunos_controller'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    hello: 'Bem vindo ao BANCO DE DADOS',
   }
 })
+router.get('alunos', [AlunosController, 'index'])
+router.post('alunos', [AlunosController, 'store'])
+router.get('alunos/:id', [AlunosController, 'show'])
+router.put('alunos/:id', [AlunosController, 'update'])
+router.delete('alunos/:id', [AlunosController, 'destroy'])

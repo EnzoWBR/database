518df6edfa5eae10873507105134c6fee76037f7
EnzoWBR <ti02.awf@gmail.com>
2024-10-03 10:11:13 -0300

    alunos funcionando

--- app/models/aluno.ts
+++ app/models/aluno.ts
@@ -2,8 +2,22 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class Aluno extends BaseModel {
+  public static table = 'alunos'
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare nome: string
+
+  @column()
+  declare endereco: string
+
+  @column()
+  declare telefone: string
+
+  @column()
+  declare email: string
+
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime

d83477dc40c9d2c177856a24aad1f5ef1c5d8cb5
EnzoWBR <ti02.awf@gmail.com>
2024-10-04 10:09:51 -0300

    aulas funcionando

--- app/controllers/aulas_controller.ts
+++ app/controllers/aulas_controller.ts
@@ -1,4 +1,93 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Aulas from '#models/aula';
+import type { HttpContext } from '@adonisjs/core/http';
 
 export default class AulasController {
+  async index({ response }: HttpContext) {
+    try {
+      const aulas = await Aulas.all();
+      if (aulas.length) {
+        response.safeStatus(200).json({
+          aulas: aulas,
+        });
+      } else {
+        response.safeStatus(200).json({
+          message: `Não há aulas para listar!`,
+        });
+      }
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao consultar as aulas! ${error}`,
+      });
+    }
+  }
+
+  async store({ request, response }: HttpContext) {
+    const { titulo, conteudo, arquivo } = request.only(['titulo', 'conteudo', 'arquivo']);
+
+    try {
+      const aulaAdd = await Aulas.create({ titulo, conteudo, arquivo });
+
+      return response.safeStatus(201).json({
+        message: `Aula ${aulaAdd.titulo}, criada com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao incluir a aula! ${error}`,
+      });
+    }
+  }
+
+  async show({ params, response }: HttpContext) {
+    try {
+      const aulaF = await Aulas.find(params.id);
+
+      if (aulaF !== null) {
+        return response.safeStatus(200).json({
+          aulaF: aulaF,
+        });
+      } else {
+        return response.safeStatus(200).json({
+          message: `Aula não localizada!`,
+        });
+      }
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao consultar a aula`,
+      });
+    }
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const aula = await Aulas.findOrFail(params.id);
+
+      const { titulo, conteudo, arquivo } = request.only(['titulo', 'conteudo', 'arquivo']);
+
+      aula.merge({ titulo, conteudo, arquivo });
+
+      await aula.save();
+
+      return response.safeStatus(201).json({
+        message: `Aula ${aula.titulo}, alterada com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao alterar a aula! ${error}`,
+      });
+    }
+  }
+
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const aula = await Aulas.findOrFail(params.id);
+      await aula.delete();
+      return response.safeStatus(200).json({
+        message: `Aula ${aula.titulo} excluída com sucesso!`,
+      });
+    } catch (error) {
+      response.safeStatus(400).json({
+        message: `Erro ao excluir a aula! ${error}`,
+      });
+    }
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,5 @@
 import AlunosController from '#controllers/alunos_controller'
+import AulasController from '#controllers/aulas_controller'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
@@ -11,3 +12,10 @@ router.post('alunos', [AlunosController, 'store'])
 router.get('alunos/:id', [AlunosController, 'show'])
 router.put('alunos/:id', [AlunosController, 'update'])
 router.delete('alunos/:id', [AlunosController, 'destroy'])
+
+
+router.get('aulas', [AulasController, 'index'])
+router.post('aulas', [AulasController, 'store'])
+router.get('aulas/:id', [AulasController, 'show'])
+router.put('aulas/:id', [AulasController, 'update'])
+router.delete('aulas/:id', [AulasController, 'destroy'])

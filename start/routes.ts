bfa4ed3840de0a548fa824c640d9f3d16c78c906
EnzoWBR <ti02.awf@gmail.com>
2024-10-17 11:46:13 -0300

    middleware funcionando perfeitamente

--- start/routes.ts
+++ start/routes.ts
@@ -5,42 +5,48 @@ import EscolasController from '#controllers/escolas_controller'
 import OrganizacaoController from '#controllers/organizacao_controller'
 import UsersController from '#controllers/users_controller'
 
-
-
 import router from '@adonisjs/core/services/router'
+import AuthMiddleware from '#middleware/auth_middleware' // Importa a classe AuthMiddleware
+
+// Instancie o middleware
+const authMiddleware = new AuthMiddleware();
 
 router.get('/', async () => {
   return {
     hello: 'Bem vindo ao BANCO DE DADOS',
   }
 })
-router.get('alunos', [AlunosController, 'index'])
-router.post('alunos', [AlunosController, 'store'])
-router.get('alunos/:id', [AlunosController, 'show'])
-router.put('alunos/:id', [AlunosController, 'update'])
-router.delete('alunos/:id', [AlunosController, 'destroy'])
 
+// Rotas de Alunos (Protegidas com middleware 'auth')
+router.get('alunos', [AlunosController, 'index']).use(authMiddleware.handle.bind(authMiddleware))
+router.post('alunos', [AlunosController, 'store']).use(authMiddleware.handle.bind(authMiddleware))
+router.get('alunos/:id', [AlunosController, 'show']).use(authMiddleware.handle.bind(authMiddleware))
+router.put('alunos/:id', [AlunosController, 'update']).use(authMiddleware.handle.bind(authMiddleware))
+router.delete('alunos/:id', [AlunosController, 'destroy']).use(authMiddleware.handle.bind(authMiddleware))
 
-router.get('aulas', [AulasController, 'index'])
-router.post('aulas', [AulasController, 'store'])
-router.get('aulas/:id', [AulasController, 'show'])
-router.put('aulas/:id', [AulasController, 'update'])
-router.delete('aulas/:id', [AulasController, 'destroy'])
+// Rotas de Aulas (Protegidas com middleware 'auth')
+router.get('aulas', [AulasController, 'index']).use(authMiddleware.handle.bind(authMiddleware))
+router.post('aulas', [AulasController, 'store']).use(authMiddleware.handle.bind(authMiddleware))
+router.get('aulas/:id', [AulasController, 'show']).use(authMiddleware.handle.bind(authMiddleware))
+router.put('aulas/:id', [AulasController, 'update']).use(authMiddleware.handle.bind(authMiddleware))
+router.delete('aulas/:id', [AulasController, 'destroy']).use(authMiddleware.handle.bind(authMiddleware))
 
+// Rotas de Escolas (Protegidas com middleware 'auth')
+router.get('escolas', [EscolasController, 'index']).use(authMiddleware.handle.bind(authMiddleware))
+router.post('escolas', [EscolasController, 'store']).use(authMiddleware.handle.bind(authMiddleware))
+router.get('escolas/:id', [EscolasController, 'show']).use(authMiddleware.handle.bind(authMiddleware))
+router.put('escolas/:id', [EscolasController, 'update']).use(authMiddleware.handle.bind(authMiddleware))
+router.delete('escolas/:id', [EscolasController, 'destroy']).use(authMiddleware.handle.bind(authMiddleware))
 
-router.get('escolas', [EscolasController, 'index'])
-router.post('escolas', [EscolasController, 'store'])
-router.get('escolas/:id', [EscolasController, 'show'])
-router.put('escolas/:id', [EscolasController, 'update'])
-router.delete('escolas/:id', [EscolasController, 'destroy'])
+// Rotas de Organização (Protegidas com middleware 'auth')
+router.get('organizacao', [OrganizacaoController, 'index']).use(authMiddleware.handle.bind(authMiddleware))
+router.post('organizacao', [OrganizacaoController, 'store']).use(authMiddleware.handle.bind(authMiddleware))
+router.get('organizacao/:id', [OrganizacaoController, 'show']).use(authMiddleware.handle.bind(authMiddleware))
+router.put('organizacao/:id', [OrganizacaoController, 'update']).use(authMiddleware.handle.bind(authMiddleware))
+router.delete('organizacao/:id', [OrganizacaoController, 'destroy']).use(authMiddleware.handle.bind(authMiddleware))
 
+// Rota de Login (Sem middleware 'auth')
+router.post('login', [AuthController, 'login'])
 
-router.get('organizacao', [OrganizacaoController, 'index'])
-router.post('organizacao', [OrganizacaoController, 'store'])
-router.get('organizacao/:id', [OrganizacaoController, 'show'])
-router.put('organizacao/:id', [OrganizacaoController, 'update'])
-router.delete('organizacao/:id', [OrganizacaoController, 'destroy'])
-
-router.post('logint',[AuthController,'login'])
-
-router.post('user',[UsersController,'store'])
+// Rota de Users (Protegida com middleware 'auth')
+router.post('user', [UsersController, 'store']).use(authMiddleware.handle.bind(authMiddleware))
